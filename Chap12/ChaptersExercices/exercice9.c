a56385d212b9e9dabb72dea4fd88ac39d3eeaf2d
Benoit Dervieux <bd222ch@student.lnu.se>
2023-08-28 15:26:33 +0200

    exercice 9 p562

--- Chap12/ChaptersExercices/exercice9.c
+++ Chap12/ChaptersExercices/exercice9.c
@@ -10,10 +10,24 @@
     int count;
     printf("Enter the number of words, 0 to quit\n");
     while (scanf("%d", &numberWord) == 1 && numberWord > 0 ) {
+        char ** array = (char **) malloc(numberWord * sizeof (char *));
         printf("Enter %d words now\n", numberWord);
         getchar();
-            while(ch=getchar() && ch != '\n') {
+        for (int i = 0; i < numberWord; i++) {
+            array[i] = (char *) malloc(40 * sizeof(char *));
+            count = 0;
+            while((ch = getchar()) && ch != '\n' && ch != ' ') {
+                //printf("The char is %c\n", ch);
+                array[i][count] = ch;
                 count++;
+            }
+            array[i][count] = '\0';
         }
+        for (int i = 0; i < numberWord; i++) {
+            printf("%s\n", array[i]);
+        }
+        printf("Enter the number of words, 0 to quit\n");
+
     }
+    
  }
